da772d1c638c4fa63df7cc0cbeed1959fa277e08
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-07-19 23:12:52 +0200

    Added previous session to users own stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,13 +14,13 @@ class StatsController < ApplicationController
   end
 
   def show
-    user_sessions = UserSession.with_user(@user).order("-created_at")
-    @session = user_sessions.first
+    @user_sessions = UserSession.with_user(@user).order("-created_at")
+    @session = @user_sessions.first
 
     if @session.present?
       @last_session_duration = @session.end_time - @session.start_time
 
-      @total_time = user_sessions.sum('TIME_TO_SEC(end_time) - TIME_TO_SEC(start_time)')
+      @total_time = @user_sessions.sum('TIME_TO_SEC(end_time) - TIME_TO_SEC(start_time)')
     end
   end
 
