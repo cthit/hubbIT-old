a02e9918c9de053310f43c63c5dc1df460d2c9da
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-08-09 02:53:01 +0200

    Bugfix for incorrect time calculation, giving negative time

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,7 +3,7 @@ class StatsController < ApplicationController
   before_action :set_user
   def index
     @sessions = UserSession.group(:user_id)
-    @total_time = @sessions.select('id','user_id','sum(TIME_TO_SEC(end_time) - TIME_TO_SEC(start_time)) as total_time').order("total_time DESC")
+    @total_time = @sessions.select('id','user_id','sum(TIMESTAMPDIFF(SECOND, `start_time`, `end_time`)) as total_time').order("total_time DESC")
   end
 
 
