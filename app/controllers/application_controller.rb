c139a791f090be005b36a428e0eeb8b793d2af00
André Samuelsson <andresamuelsson94@gmail.com>
2015-08-29 02:30:24 +0200

    Redirect fix if no auth-cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  rescue_from SecurityError, with: :not_signed_in
+
   def current_user
+    if not cookies[:chalmersItAuth].present?
+      raise SecurityError, "Missing cookie"
+    end
     if session[:cookie] == cookies[:chalmersItAuth] && session[:user].present?
       @current_user ||= User.find(session[:user])
     else
@@ -15,5 +20,10 @@ class ApplicationController < ActionController::Base
     @current_user
   end
   helper_method :current_user
+
+  private
+    def not_signed_in
+      redirect_to "https://account.chalmers.it"
+    end
 
 end
