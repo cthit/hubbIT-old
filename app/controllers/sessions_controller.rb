a3d479abbdef233213ac3cf4a021e00fc008ec56
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-09-10 14:33:24 +0200

    remove sort error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   @@semaphore = Mutex.new
 
   def index
-    @sessions = UserSession.active.includes(:user).sort_by(&:nick)
+    @sessions = UserSession.active.includes(:user)
     @groups = map_users_to_groups(@sessions.map { |s| {user: s.user, groups: s.user.groups} })
 
     respond_to do |format|
