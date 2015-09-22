7395f9ef3e4320e5b5c46aec4d57ed42864700f3
Emil Edholm <bobby@verypublic.org>
2015-09-22 19:28:56 +0200

    Styling of Me page

--- db/migrate/20150915161752_add_device_name_to_mac_addresses.rb
+++ db/migrate/20150915161752_add_device_name_to_mac_addresses.rb
@@ -1,5 +1,5 @@
 class AddDeviceNameToMacAddresses < ActiveRecord::Migration
   def change
-  	add_column :mac_addresses, :device_name, :string
+    add_column :mac_addresses, :device_name, :string
   end
 end
