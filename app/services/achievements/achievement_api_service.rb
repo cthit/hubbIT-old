753acea413423db80405ff758fee99ca67a58aeb
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-10-06 23:46:54 +0200

    Changed indentation to spaces

--- app/services/achievements/achievement_api_service.rb
+++ app/services/achievements/achievement_api_service.rb
@@ -1,40 +1,40 @@
 class AchievementApiService
-	include HTTParty
+    include HTTParty
     #debug_output $stdout
-	base_uri "https://achieveit.chalmers.it"
+    base_uri "https://achieveit.chalmers.it"
 
     ROOT_URL = "https://hubbit.chalmers.it"
 
-	ACHIEVEMENT_URI = "/achievements.json"
-	UNLOCK_URI = "/unlocks.json"
+    ACHIEVEMENT_URI = "/achievements.json"
+    UNLOCK_URI = "/unlocks.json"
     API_TOKEN = Rails.application.secrets.achievement_api_key
 
-	def self.submit_achievement(name, code, icon="", description="", category="", points=1)
-    	body = { 
-				name: name,
+    def self.submit_achievement(name, code, icon="", description="", category="", points=1)
+        body = { 
+                name: name,
                 code: code,
-				icon: ROOT_URL + icon,
-				desc: description,
-				category: category,
-				points: points
+                icon: ROOT_URL + icon,
+                desc: description,
+                category: category,
+                points: points
              }
 
         AchievementApiService.submit(ACHIEVEMENT_URI, achievement: body)
     end
 
     def self.unlock_achievement(cid, achievement_code)
-    	body = { 
-			cid: cid,
-			code: achievement_code
-    	}
+        body = { 
+            cid: cid,
+            code: achievement_code
+        }
 
-    	AchievementApiService.submit(UNLOCK_URI, unlock: body)
+        AchievementApiService.submit(UNLOCK_URI, unlock: body)
     end
 
    private
 
-   	def self.submit(uri, body)
-   		post(uri, 
+    def self.submit(uri, body)
+        post(uri, 
             :body => body.to_json,
             :headers => { 
                 'Content-Type' => 'application/json', 
