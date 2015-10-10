b085b39aab7f5d4ca8b5cc7f2622e49a38c5e866
Emil Edholm <bobby@verypublic.org>
2015-10-10 16:35:16 +0200

    Correctly calculate longest session. Fixes #68

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -52,7 +52,7 @@ class StatsController < ApplicationController
   def show
     @user_sessions = UserSession.with_user(@user).order("-created_at")
     @session = @user_sessions.first
-    @longest_session = @user_sessions.maximum('TIME_TO_SEC(end_time) - TIME_TO_SEC(start_time)')
+    @longest_session = @user_sessions.longest_session.first.longest_session
 
     if @session.present?
       @last_session_duration = @session.end_time - @session.start_time
