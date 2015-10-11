1ab12788798a62c05dcd14bab12716ea12423d9b
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-10-11 20:23:01 +0200

    Resolved conflict

--- app/services/achievements/hourly/first.rb
+++ app/services/achievements/hourly/first.rb
@@ -3,12 +3,7 @@ require_relative '../achievement'
 class First < Achievement
 
     def initialize
-<<<<<<< Updated upstream
-        super 'First', 'first', 'Place yourself first', 'No life', 500
-        p 'init first'
-=======
         super 'First!!!11eleven', 'first', 'Place yourself first', 'No life', 500
->>>>>>> Stashed changes
     end
 
     def achieved? user
