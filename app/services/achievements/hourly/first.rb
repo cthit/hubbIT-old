32796a8c99284a2be508a6f6869df6832f1a9758
Johan 'Horv' Andersson <drhorv@gmail.com>
2015-10-11 20:21:53 +0200

    Updated first name

--- app/services/achievements/hourly/first.rb
+++ app/services/achievements/hourly/first.rb
@@ -3,13 +3,17 @@ require_relative '../achievement'
 class First < Achievement
 
     def initialize
+<<<<<<< Updated upstream
         super 'First', 'first', 'Place yourself first', 'No life', 500
         p 'init first'
+=======
+        super 'First!!!11eleven', 'first', 'Place yourself first', 'No life', 500
+>>>>>>> Stashed changes
     end
 
     def achieved? user
         return false if user.users_total_time.nil?
-        user.ranking <= 1
+        user.ranking == 1
     end
 
 end
