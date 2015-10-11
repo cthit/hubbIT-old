752def6638e1effadfeee55054e4ca9a85fdd985
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-10-11 20:49:30 +0200

    Over nine thousand video

--- app/services/achievements/hourly/over_nine_thousand.rb
+++ app/services/achievements/hourly/over_nine_thousand.rb
@@ -3,7 +3,7 @@ require_relative '../achievement'
 class OverNineThousand < Achievement
 
     def initialize
-        super 'Over 9000', 'over_nine_thousand', 'Log over 9000 hours in the Hubb', 'Insane', 1337
+        super 'Over 9000', 'over_nine_thousand', 'Log over 9000 hours in the Hubb [youtube.com/watch?v=SiMHTK15Pik](https://www.youtube.com/watch?v=SiMHTK15Pik)', 'Insane', 1337
     end
 
     def achieved? user
