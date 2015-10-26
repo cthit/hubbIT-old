d0e7e0cbe9ece984d953f75405172f8876d76e49
Pontus Malm <malm@chalmers.it>
2015-10-26 12:06:46 +0100

    Adds Valberedningen to groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
 
 	delegate :ranking, to: :users_total_time
 
-	ALLOWED_GROUPS = [:styrit, :snit, :sexit, :prit, :nollkit, :armit, :digit, :fanbarerit, :fritid, :'8bit', :drawit, :flashit, :hookit, :revisorer, :valberedning]
+	ALLOWED_GROUPS = [:styrit, :snit, :sexit, :prit, :nollkit, :armit, :digit, :fanbarerit, :fritid, :'8bit', :drawit, :flashit, :hookit, :revisorer, :valberedningen]
 
 	self.primary_key = :cid
 
@@ -92,6 +92,8 @@ class Symbol
 				'P.R.I.T.'
 			when :fanbarerit
 				'FanbärerIT'
+			when :valberedningen
+				'Valberedningen'
 			else
 				self.to_s
 		end
