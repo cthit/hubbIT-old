51f040343be63b851af8f63d7c858d8af4be788b
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-11-17 21:11:23 +0100

    Catch SecurityError model, not string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  rescue_from 'SecurityError' do |error|
+  rescue_from SecurityError do |error|
     respond_to do |format|
       format.json { render json: {error: error}, status: :forbidden }
       format.html { not_signed_in }
