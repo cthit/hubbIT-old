44261cec8bcaba3e50881c92b62d7a7c94f9305f
Pontus Malm <malm@chalmers.it>
2018-05-04 19:43:29 +0200

    Time for user session tests

--- spec/requests/user_session_spec.rb
+++ spec/requests/user_session_spec.rb
@@ -31,4 +31,44 @@ describe "User session" do
     expect(UserSession.active.size).to eq 1
     expect(UserSession.active.with_user(user).size).to eq 1
   end
+
+  it "to be updated by another device" do
+    user = create(:user_with_devices)
+    device1, device2 = user.devices
+
+    put "/sessions", params: {macs: [device1.address]}, headers: @headers
+    expect(UserSession.active.with_user(user).size).to eq 1
+    Timecop.freeze(8.minutes.from_now) do
+      put "/sessions", params: {macs: [device2.address]}, headers: @headers
+      Timecop.freeze(8.minutes.from_now) do
+        expect(UserSession.with_user(user).size).to eq 1
+        expect(UserSession.active.with_user(user).size).to eq 1
+      end
+    end
+  end
+
+  it "to expire after some time" do
+    device = create(:device)
+    user = device.user
+
+    put "/sessions", params: {macs: [device.address]}, headers: @headers
+    expect(UserSession.active.with_user(user).size).to eq 1
+    Timecop.freeze(15.minutes.from_now) do
+      expect(UserSession.with_user(user).size).to eq 1
+      expect(UserSession.active.with_user(user)).to be_empty
+    end
+  end
+
+  it "to be created if the last user session expires" do
+    device = create(:device)
+    user = device.user
+
+    put "/sessions", params: {macs: [device.address]}, headers: @headers
+    expect(UserSession.active.with_user(user).size).to eq 1
+    Timecop.freeze(15.minutes.from_now) do
+      put "/sessions", params: {macs: [device.address]}, headers: @headers
+      expect(UserSession.with_user(user).size).to eq 2
+      expect(UserSession.active.with_user(user).size).to eq 1
+    end
+  end
 end
