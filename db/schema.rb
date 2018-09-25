aaca166589e141d73e9b132cdff00e2ccbcaab94
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-09-25 23:26:24 +0200

    rewrite how container is started
    the container now properly waits for the DB to start

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,65 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915161752) do
+ActiveRecord::Schema.define(version: 2015_09_15_161752) do
 
-  create_table "api_keys", force: true do |t|
-    t.string   "access_token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "comment"
+  create_table "api_keys", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "access_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "comment"
   end
 
-  create_table "hour_entries", force: true do |t|
-    t.string   "cid"
-    t.date     "date"
-    t.integer  "hour"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "hour_entries", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "cid"
+    t.date "date"
+    t.integer "hour"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "mac_addresses", id: false, force: true do |t|
-    t.string   "address"
-    t.string   "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "device_name"
+  create_table "mac_addresses", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "address"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "device_name"
+    t.index ["user_id"], name: "index_mac_addresses_on_user_id"
   end
 
-  add_index "mac_addresses", ["address"], name: "address", unique: true, using: :btree
-  add_index "mac_addresses", ["user_id"], name: "index_mac_addresses_on_user_id", using: :btree
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.timestamp "start_time"
+    t.timestamp "end_time"
+    t.string "mac_address"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mac_address"], name: "index_sessions_on_mac_address"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
-  create_table "sessions", force: true do |t|
+  create_table "user_sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
-    t.string   "mac_address"
-    t.string   "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_sessions_on_user_id"
   end
 
-  add_index "sessions", ["user_id"], name: "index_sessions_on_user_id", using: :btree
-
-  create_table "user_sessions", force: true do |t|
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.string   "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "user_sessions", ["user_id"], name: "index_user_sessions_on_user_id", using: :btree
-
-  create_table "users", id: false, force: true do |t|
-    t.string   "cid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users_total_time", id: false, force: true do |t|
-    t.integer "id",                                  default: 0, null: false
-    t.string  "user_id"
-    t.decimal "total_time", precision: 42, scale: 0
+  create_table "users", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "cid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- dev.Dockerfile
+++ dev.Dockerfile
@@ -2,7 +2,7 @@ FROM ruby:2.5.1
 #
 RUN apt-get update && apt-get install -y \
 #Packages
-net-tools
+net-tools netcat
 
 RUN apt-get clean
 
