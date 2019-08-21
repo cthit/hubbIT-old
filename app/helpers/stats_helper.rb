cbfbf361fdd94fad646a955202e0e10d84198d5f
gurr1 <gustav.engsmyre@gmail.com>
2019-08-21 13:36:10 +0200

    add time periods of 2019-20 study year

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -5,6 +5,7 @@ module StatsHelper
         [Time.new(2016,8,16), Time.new(2017,8,15)],
         [Time.new(2017,8,16), Time.new(2018,8,15)],
         [Time.new(2018,8,16), Time.new(2019,8,15)],
+        [Time.new(2019,8,16), Time.new(2020,8,15)],
     ]
 
     @@study_period = [
@@ -36,11 +37,18 @@ module StatsHelper
         
         [Time.new(2019,6,9), Time.new(2019,9,3)], # Summer break
 
+        [Time.new(2019,9,3), Time.new(2019,11,4)], #LP1
+        [Time.new(2019,11,5), Time.new(2020,1,20)], #LP2
+        [Time.new(2020,1,21), Time.new(2020,3,24)], #LP3
+        [Time.new(2020,3,25), Time.new(2020,6,8)], #LP4
+
     ]
 
     def get_current_study_year_index()
+      print "go here"
         @@study_years.each_with_index do |study_year, index|
             if study_year[0].beginning_of_day <= Time.now && Time.now <= study_year[1].end_of_day
+              puts(index)
                 return index
             end
         end
